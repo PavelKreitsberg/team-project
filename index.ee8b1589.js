94e0d116bb74f1623408bcc73f406120d698aee9
PavelKreitsberg <99120090+PavelKreitsberg@users.noreply.github.com>
2022-06-01 05:29:16 +0000

    Deploying to gh-pages from @ PavelKreitsberg/team-project@9a0c17ad9188704c47fd01fd44aa8926b764ccd2 🚀

--- index.ee8b1589.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMenuBtn:document.querySelector(".menu-open_btn "),closeMenuBtn:document.querySelector(".menu-close_btn "),menu:document.querySelector(".mob-menu")};function n(){e.menu.classList.toggle("is-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.ee8b1589.js.map
